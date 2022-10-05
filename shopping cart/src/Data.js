b576229fc9fe0aaf69a807c0afc0ad44d0a302e6
nsikak100 <nsikak.i.m@gmail.com>
2022-10-05 23:16:18 +0100

    first commit

---
+++ shopping cart/src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "anything1",
+    name: "Nike black",
+    price: 2800,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "anything2",
+    name: "Plasma",
+    price: 1650,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "anything3",
+    name: "Reeboks",
+    price: 3555,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "anything4",
+    name: "Kixxs",
+    price: 4689,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-4.jpg",
+  },
+];
